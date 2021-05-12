f99e095c3ddb146520197bb0438765d9af85085d
encounter115 <1459722320@qq.com>
2021-05-12 22:23:40 +0800

    闭包2

--- day02/2.js
+++ day02/2.js
@@ -115,18 +115,18 @@
 // }
 // B();
 
-let x = 5;
-function fn(x) {
-    return function(y) {
-        console.log(y + (++x));
-    }
-}
-let f = fn(6);
-f(7)
-fn(8)(9);
-f(10);
-console.log(x);
-
+// let x = 5;
+// function fn(x) {
+//     return function(y) {
+//         console.log(y + (++x));
+//     }
+// }
+// let f = fn(6); 
+// f(7)  // 14
+// fn(8)(9); //18
+// f(10); //18 
+// console.log(x); // 5
+// -> 14 18 18 5
 // ++x & x++
 //  ++x:先累加1，用累加后的结果再去运算
 //  x++:先拿原始值去运算，运算结束后，自身累加1
@@ -139,3 +139,22 @@ console.log(x);
 // let i = 1;
 // console.log(5 + (i)); //-> 6
 // console.log(i) //-> 2
+
+// EC(G)
+//  x=10
+//  fn = 0x000 [[scope]:EC(G)]
+let x= 10;
+function fn() {
+    // EC(FN) 闭包
+    //  y = 20
+    //  f=0x001[[scope]:EC(FN)]
+    let y = 20;
+    function f() {
+        console.log(y)
+    }
+    window.f = f;
+}
+fn();
+fn = null; // 把0x000堆释放掉,但是之前形成上下文不受影响
+f(); // ->20
+window.f = null //把0x001堆释放掉，此时EC(FN)中没有被外面占用的内容了，它也被释放掉
